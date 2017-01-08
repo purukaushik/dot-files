dcb2a5f38a6e4785ee50e927e72306073b28cd56
Purush Swaminathan <purukaushik@asu.edu>
2017-01-08 10:40:32 -0700

    Update scalar.sh

--- scalar.sh
+++ scalar.sh
@@ -1,2 +1,13 @@
 #!/bin/zsh
-scalac $1.scala && scala $1
+#Script to compile and run a scala program with command args.
+if [ $# -eq 0 ]
+  then
+    echo "Usage: scalar ClassName [PROGRAM-ARGS]"
+    exit
+fi
+if [ ! -z "${@:2}" ]
+  then
+	scalac $1.scala && scala $1 ${@:2}
+  else
+   	scalac $1.scala && scala $1
+fi
