76db5b7c5496f92dc647c0f6dc064f7663cf1a15
Purush Swaminathan <purukaushik@asu.edu>
2017-02-08 10:31:12 -0700

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,6 +5,11 @@
                      ("melpa" . "https://melpa.org/packages/")))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+;; default encoding
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
 (setq package-list '(autopair el-get form-feed gist gh logito pcache git f dash s dash s git-gutter+ dash git-commit with-editor dash async dash idle-highlight-mode jedi auto-complete popup jedi-core python-environment deferred epc ctable concurrent deferred jedi-core python-environment deferred epc ctable concurrent deferred js2-mode json-mode json-snatcher json-reformat json-reformat json-snatcher logito lua-mode markdown-mode pcache popup python-environment deferred s sublime-themes web-mode with-editor dash async yasnippet solarized))
 ;; The packages you want installed. You can also install these
 ;; manually with M-x package-install
@@ -12,7 +17,7 @@
 (defvar my-packages
   '(;; makes handling lisp expressions much, much easier
     ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
-    paredit
+    ;;paredit
 
     ;; key bindings and code colorization for Clojure
     ;; https://github.com/clojure-emacs/clojure-mode
@@ -28,7 +33,7 @@
     ;; allow ido usage in as many contexts as possible. see
     ;; customizations/navigation.el line 23 for a description
     ;; of ido
-    ido-ubiquitous
+    ;;ido-ubiquitous
 
     ;; Enhances M-x to allow easier execution of commands. Provides
     ;; a filterable list of possible commands in the minibuffer
@@ -57,19 +62,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
+ '(cursor-type (quote (hbar . 10)))
  '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
    (quote
     ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(inhibit-startup-screen t)
  '(jdee-server-dir "~/.emacs.d/")
+ '(shell-file-name "/usr/bin/zsh")
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Fira Mono" :foundry "unknown" :slant normal :weight normal :height 120 :width normal))))
+ '(cursor ((t (:background "firebrick" :foreground "#fdf6e3" :inverse-video t)))))
 (package-initialize)
 ;; THEME LOAD
 (load-theme 'solarized-light t)
@@ -86,6 +94,9 @@
 (global-set-key (kbd "C-z") 'undo )
 (global-set-key (kbd "C-1") 'delete-other-windows )
 (global-set-key (kbd "C-x g") 'magit-status)
+;;JDEE Key set
+(eval-after-load 'jdee
+  '(local-set-key (kbd "C-c C-v C-i") 'jdee-import-all))
 ;; JAVA INDENT
 (add-hook 'java-mode-hook (lambda ()
 			    (setq c-basic-offset 2)))
@@ -152,4 +163,25 @@
 (load "setup-js.el")
 
 ;; Window resize on init
-(when window-system (set-frame-size (selected-frame) 115 65))
+;;(when window-system (set-frame-size (selected-frame) 115 65))
+
+;; centered typewriter mode
+;;(global-centered-cursor-mode t)
+
+;; transparency
+(defun toggle-transparency ()
+   (interactive)
+   (let ((alpha (frame-parameter nil 'alpha)))
+     (set-frame-parameter
+      nil 'alpha
+      (if (eql (cond ((numberp alpha) alpha)
+                     ((numberp (cdr alpha)) (cdr alpha))
+                     ;; Also handle undocumented (<active> <inactive>) form.
+                     ((numberp (cadr alpha)) (cadr alpha)))
+               100)
+          '(75 . 55) '(100 . 100)))))
+ (global-set-key (kbd "C-c t") 'toggle-transparency)
+
+;; default buffer
+(setq initial-major-mode 'org-mode)
+(setq initial-scratch-message nil)
